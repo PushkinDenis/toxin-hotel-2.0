01dcc787501aedc1b3fc0ec1e4a8429c5564438c
PushkinDenis <denisp9831@gmail.com>
2023-02-12 20:51:09 +1000

    counter component fixed
    counter component fixed

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,15 +1,25 @@
-const counter = document.querySelector(".counter-wrapper");
-const cntMinusBtn = document.querySelector(".counter__minus-button");
-const cntPlusBtn = document.querySelector(".counter__plus-button");
-let counterValue = document.querySelector(".counter__value");
+const counter = document.querySelector(".counter-wrapper"); ///поменять на id
+const cntMinusBtn = document.querySelector(".counter_minus-btn");
+const cntPlusBtn = document.querySelector(".counter_plus-btn");
+let counterInput = document.querySelector(".counter__value");
 
 counter.addEventListener("click", (event) => {
-  if (event.target.className === "counter__minus-button") {
-    counterValue.value--;
-    counterValue.value = counterValue.value < 0 ? 0 : counterValue.value;
-    console.log(counterValue.value);
-  } else if (event.target.className === "counter__plus-button") {
-    counterValue.value++;
-    console.log(counterValue.value);
+  if (event.target.dataset.counterMinusBtn) {
+    const { value } = counterInput;
+    const numberValue = Number(value);
+    if (numberValue === 1) {
+      cntMinusBtn.classList.add("counter_inactive");
+    } else if (numberValue === 0) {
+      return;
+    }
+
+    counterInput.value = String(numberValue - 1);
+    console.log(counterInput.value);
+  } else if (event.target.dataset.counterPlusBtn) {
+    cntMinusBtn.classList.remove("counter_inactive");
+    const { value } = counterInput;
+    const numberValue = Number(value);
+    counterInput.value = String(numberValue + 1);
+    console.log(counterInput.value);
   }
 });
