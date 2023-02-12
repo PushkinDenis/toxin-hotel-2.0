90b435f3537f985854eaf24ebe26c5acffd8356c
PushkinDenis <denisp9831@gmail.com>
2023-02-12 21:25:13 +1000

    select-component fixed
    select-component fixed

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,26 +1,6 @@
-// import { createPopper, flip } from "@popperjs/core";
-//
-const selectInput = document.querySelector(".input-field__input-select");
+const selectInput = document.querySelector(".input-field__select");
 const selectList = document.querySelector(".select__list");
 
-// const popperInstance = createPopper(button, tooltip, {
-//   modifiers: [],
-//   placement: "bottom",
-// });
-
-// let dropdownShow = () => {
-//   let tooltipClass = tooltip.className;
-//   if (tooltipClass === "dropdown__list") {
-//     tooltip.classList.remove("dropdown__list");
-//     tooltip.classList.add("dropdown__list_active");
-//   } else {
-//     tooltip.classList.remove("dropdown__list_active");
-//     tooltip.classList.add("dropdown__list");
-//   }
-// };
-// let selectListShow = () => {
-//   selectList.classList.toggle("_hided", false);
-// };
 selectInput.addEventListener("click", () => {
   selectList.classList.toggle("select_hided");
 });
