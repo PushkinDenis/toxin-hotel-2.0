603cdb05204fa1a4fd54b7dea296f80edad3b1ca
PushkinDenis <denisp9831@gmail.com>
2023-02-27 19:06:52 +1000

    Select-component fixed

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,6 +1,20 @@
-const selectInput = document.querySelector(".input-field__select");
-const selectList = document.querySelector(".select__list");
-
-selectInput.addEventListener("click", () => {
-  selectList.classList.toggle("select_hided");
+const selectListArr = document.querySelectorAll("[data-list-id]");
+const selectInputArr = document.querySelectorAll("[data-select-id]");
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.selectId) {
+    const selectId = event.target.dataset.selectId;
+    const listId = selectListArr[+selectId].dataset.listId;
+    if (selectId === listId) {
+      const targetSelect = document.querySelector(
+        `[data-list-id="${String(selectId)}"]`
+      );
+      if (targetSelect.className === "select_hided") {
+        targetSelect.classList.remove("select_hided");
+        targetSelect.classList.add("select__list");
+      } else {
+        targetSelect.classList.remove("select__list");
+        targetSelect.classList.add("select_hided");
+      }
+    }
+  }
 });
