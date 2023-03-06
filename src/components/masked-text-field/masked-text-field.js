2e0cc632ee18d780daabe16a5696a51fab241817
PushkinDenis <denisp9831@gmail.com>
2023-03-06 19:56:20 +1000

    masked-text-field component created

---
+++ src/components/masked-text-field/masked-text-field.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  jQuery(function ($) {
+    $(".input-field_masked").mask("99.99.9999", { placeholder: "ДД.ММ.ГГГГ" });
+  });
+});
--- src/pages/ui-kit-form-elements/index.js
+++ src/pages/ui-kit-form-elements/index.js
@@ -1,3 +1,11 @@
 import("../../components/select/select");
 import("../../components/checkbox/checkbox");
 import("../../components/counter/counter");
+import("../../components/masked-text-field/masked-text-field");
+require("jquery.maskedinput/src/jquery.maskedinput");
+// $(".block").html("vsvsvsvsvs");
+// $(document).ready(function () {
+//   jQuery(function ($) {
+//     $("#block").mask("99.99.9999", { placeholder: "ДД.ММ.ГГГГ" });
+//   });
+// });
