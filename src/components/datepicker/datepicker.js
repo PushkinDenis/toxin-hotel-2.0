c5d3b5d46ac8a127f4a528fa3b903b3403727edf
PushkinDenis <denisp9831@gmail.com>
2023-04-17 20:41:28 +1000

    datepicker component fixed

--- src/components/datepicker/datepicker.js
+++ src/components/datepicker/datepicker.js
@@ -1,27 +1,38 @@
+// const ar = document.querySelector(".ar");
+import startDay from "daterangepicker";
+const startDate = document.querySelector(".input-field__start-date");
+const endDate = document.querySelector(".input-field__end-date");
 $(function () {
-  $(".input-field__datepicker").daterangepicker({
-    locale: {
-      format: "DD.MM.YYYY",
-      daysOfWeek: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
-      monthNames: [
-        "Январь",
-        "Февраль",
-        "Март",
-        "Апрель",
-        "Май",
-        "Июнь",
-        "Июль",
-        "Август",
-        "Сентябрь",
-        "Октябрь",
-        "Ноябрь",
-        "Декабрь",
-      ],
-      firstDay: 1,
-      cancelLabel: "ОЧИСТИТЬ",
-      applyLabel: "ПРИМЕНИТЬ",
+  const datepicker = $(".input-field__datepicker-wrapper").daterangepicker(
+    {
+      locale: {
+        format: "DD.MM.YYYY",
+        daysOfWeek: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        monthNames: [
+          "Январь",
+          "Февраль",
+          "Март",
+          "Апрель",
+          "Май",
+          "Июнь",
+          "Июль",
+          "Август",
+          "Сентябрь",
+          "Октябрь",
+          "Ноябрь",
+          "Декабрь",
+        ],
+
+        firstDay: 1,
+        cancelLabel: "ОЧИСТИТЬ",
+        applyLabel: "ПРИМЕНИТЬ",
+      },
     },
-  });
+    function () {
+      startDate.value = this.startDate.format("DD.MM.YYYY");
+      endDate.value = this.endDate.format("DD.MM.YYYY");
+    }
+  );
   let arrow =
     this.locale.direction == "ltr"
       ? { left: "chevron-right", right: "chevron-right" }
