3d37a73655a92c870399fec4250c0965f6b142a7
PushkinDenis <denisp9831@gmail.com>
2023-05-14 12:51:29 +1000

    select and counter components fixed

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,25 +1,47 @@
-const counter = document.querySelector(".counter-wrapper"); ///поменять на id
-const cntMinusBtn = document.querySelector(".counter_minus-btn");
-const cntPlusBtn = document.querySelector(".counter_plus-btn");
-let counterInput = document.querySelector(".counter__value");
-
-counter.addEventListener("click", (event) => {
-  if (event.target.dataset.counterMinusBtn) {
-    const { value } = counterInput;
-    const numberValue = Number(value);
-    if (numberValue === 1) {
-      cntMinusBtn.classList.add("counter_inactive");
-    } else if (numberValue === 0) {
-      return;
+const counters = document.querySelectorAll("[data-counter]");
+let totalCnt = 0;
+counters.forEach((counter) => {
+  const selInpElem =
+    counter.previousSibling.parentNode.previousSibling.parentNode
+      .previousSibling.parentNode.firstChild.lastChild.firstChild;
+  let cntValue = 0;
+  const cntMinusBtn = counter.firstChild.nextSibling;
+  const cntPlusBtn =
+    counter.firstChild.nextSibling.nextSibling.nextSibling.nextSibling;
+  const cntInpVal = counter.firstChild.nextSibling.nextSibling;
+  cntMinusBtn.addEventListener("click", (event) => {
+    if (cntInpVal.value > 0) {
+      cntInpVal.value = --cntValue;
+      --totalCnt;
+      selInpElem.value = `${totalCnt} гостя`;
     }
+    if (totalCnt > 0) {
+      counter.parentNode.parentNode.lastChild.previousSibling.firstChild.nextSibling.textContent =
+        "ОЧИСТИТЬ";
+    } else {
+      counter.parentNode.parentNode.lastChild.previousSibling.firstChild.nextSibling.textContent =
+        "";
+    }
+  });
+  cntPlusBtn.addEventListener("click", (event) => {
+    cntInpVal.value = ++cntValue;
+    ++totalCnt;
+    selInpElem.value = `${totalCnt} гостя`;
 
-    counterInput.value = String(numberValue - 1);
-    console.log(counterInput.value);
-  } else if (event.target.dataset.counterPlusBtn) {
-    cntMinusBtn.classList.remove("counter_inactive");
-    const { value } = counterInput;
-    const numberValue = Number(value);
-    counterInput.value = String(numberValue + 1);
-    console.log(counterInput.value);
-  }
+    if (totalCnt > 0) {
+      counter.parentNode.parentNode.lastChild.previousSibling.firstChild.nextSibling.textContent =
+        "ОЧИСТИТЬ";
+    } else {
+      counter.parentNode.parentNode.lastChild.previousSibling.firstChild.nextSibling.textContent =
+        "";
+    }
+  });
+  let clrBtn =
+    counter.parentNode.parentNode.lastChild.previousSibling.firstChild
+      .nextSibling;
+  clrBtn.addEventListener("click", () => {
+    selInpElem.value = 0;
+    totalCnt = 0;
+    cntValue = 0;
+  });
 });
--- src/components/select/select.js
+++ src/components/select/select.js
@@ -4,6 +4,7 @@ const selectInput = document.querySelectorAll("[data-select-input]");
 selectInput.forEach((elem) => {
   elem.addEventListener("click", () => {
     const elemList = elem.parentNode.parentNode.nextSibling.nextSibling;
+    const addBtn = elemList.lastChild.previousSibling.lastChild.previousSibling;
     if (elemList.className === "select_hided") {
       elemList.classList.remove("select_hided");
       elemList.classList.add("select__list");
@@ -11,5 +12,9 @@ selectInput.forEach((elem) => {
       elemList.classList.remove("select__list");
       elemList.classList.add("select_hided");
     }
+    addBtn.addEventListener("click", () => {
+      elemList.classList.remove("select__list");
+      elemList.classList.add("select_hided");
+    });
   });
 });
